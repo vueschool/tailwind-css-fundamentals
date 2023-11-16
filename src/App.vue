b87f97f19d7fc0a39717a6995a0b07675e303468
Daniel Kelly <me@danielkelly.io>
2023-11-16 16:28:41 -0600

    5-dark-mode-in-tailwind-css

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 import AppButton from "./components/AppButton.vue";
 import AppInput from "./components/AppInput.vue";
 import TheMainNav from "./components/TheMainNav.vue";
+import { useDark } from "@vueuse/core";
+
+const isDark = useDark();
 </script>
 <template>
   <div class="m-10">
+    <input type="checkbox" v-model="isDark" />Dark Mode
     <ul class="list-disc marker:text-green-500 marker:text-2xl">
       <li>Vue.js</li>
       <li>React</li>
