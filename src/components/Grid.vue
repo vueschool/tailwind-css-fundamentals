58f3ef1190da08c41385b89c79cb7c8ab69a0bd9
Daniel Kelly <me@danielkelly.io>
2023-11-28 16:02:40 -0600

    7-1-mixing-flex-and-grid-plus-important

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -2,7 +2,12 @@
   <section class="grid grid-cols-4 gap-2 grid-flow-dense">
     <div></div>
     <div></div>
-    <div class="col-span-3"></div>
+    <div class="flex items-center justify-around col-span-3">
+      <div class="w-16 h-16 min-h-0 bg-green-300"></div>
+      <div class="w-16 h-16 min-h-0 bg-blue-300"></div>
+      <div class="w-16 h-16 min-h-0 bg-green-300"></div>
+      <div class="w-16 h-16 min-h-0 bg-blue-300"></div>
+    </div>
     <div></div>
     <div></div>
     <div></div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  important: true,
   // darkMode: "class",
 };
